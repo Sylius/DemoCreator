a0548854c12926efbcd4e475607ffd7b7bca8af1
Rafikooo <thecrazyrafik@gmail.com>
2025-05-21 09:20:09 +0200

    Add demo config provider

---
+++ src/Service/Provider/DemoConfigProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service\Provider;
+
+final class DemoConfigProvider
+{
+    public function getPlugins(): array
+    {
+        return [
+            'sylius/b2b-kit',
+            'sylius/return-plugin',
+            'sylius/cms-plugin',
+            'sylius/invoicing-plugin',
+        ];
+    }
+
+    public function getFixtures(): array
+    {
+        return [
+            'tractors',
+        ];
+    }
+
+    public function getLogo(): array
+    {
+        return [
+            'tractor',
+        ];
+    }
+
+    public function getDeployTargets(): array
+    {
+        return [
+            'platform.sh',
+        ];
+    }
+}
