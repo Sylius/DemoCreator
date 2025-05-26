7b70e2591f766839f1e6efd0909203cfa6be6340
Rafikooo <thecrazyrafik@gmail.com>
2025-05-26 15:35:51 +0200

    WIP

---
+++ src/Controller/ThemePreviewController.php
@@ -0,0 +1,36 @@
+<?php
+// src/Controller/ThemePreviewController.php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route(
+    path: '/theme/preview/{section}',
+    name: 'app_theme_preview',
+    requirements: ['section' => 'admin|shop'],
+    methods: ['GET']
+)]
+final class ThemePreviewController extends AbstractController
+{
+    public function __invoke(Request $request, string $section = 'admin'): Response
+    {
+        $variables = [
+            '--tblr-primary'       => $request->query->get('primary', '#206bc4'),
+            '--tblr-btn-bg'        => $request->query->get('btnBg', '#206bc4'),
+            '--tblr-btn-hover-bg'  => $request->query->get('btnHover', '#206bc4'),
+        ];
+
+        // Forward the base URL for preview (port 8000)
+        $previewBaseUrl = 'http://localhost:8000';
+
+        return $this->render('theme_preview/index.html.twig', [
+            'section'        => $section,
+            'variables'      => $variables,
+            'previewBaseUrl' => $previewBaseUrl,
+        ]);
+    }
+}
