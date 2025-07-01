d05ba4a495491a6adcae3e8d6b855f85e43bf34c
Rafikooo <thecrazyrafik@gmail.com>
2025-07-02 01:41:10 +0200

    WIP

--- assets/react/controllers/DemoWizard.jsx
+++ assets/react/controllers/DemoWizard.jsx
@@ -306,7 +306,6 @@ export default function DemoWizard({
         setStoreDetails(null);
         setIsDescribeStoreStageReady(false);
         setIsFixturesGenerating(false);
-        setFixturesError(null);
         setIsFixturesReady(false);
         setDeployStateId(null);
         setDeployUrl(null);
