502965292af7f20ba21286a421636155e3351804
Rafikooo <thecrazyrafik@gmail.com>
2025-07-16 22:38:45 +0200

    WIP

--- src/StoreDesigner/Controller/DeployStateController.php
+++ src/StoreDesigner/Controller/DeployStateController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\StoreDesigner\Controller;
 
 use App\Service\DemoDeployer\PlatformShDeployer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -14,7 +14,7 @@ final class DeployStateController extends AbstractController
     {
     }
 
-    #[Route('/deploy-state/{environment}/{activityId}', name: 'demo_deploy_state', methods: ['GET'])]
+    #[Route('/api/deploy-state/{environment}/{activityId}', name: 'demo_deploy_state', methods: ['GET'])]
     public function index(string $environment, string $activityId): JsonResponse
     {
         $deployState = $this->deployer->getDeployState($environment, $activityId);
